23056636c7065439b2bc027772ecb397cc54ff59
Samyak Singh <54956876+SAMYAK99@users.noreply.github.com>
2020-07-15 03:07:32 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,42 @@
+import 'package:dynamic_theme/dynamic_theme.dart';
 import 'package:flutter/material.dart';
-import 'package:newsxflutter/views/home.dart';
+import 'package:newsxflutter/views/SplashScreen.dart';
 
+void main() {
+  runApp(MyApp());
+}
 
-void main() => runApp(MyApp());
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
+class _MyAppState extends State<MyApp> {
+  void changeBrightness() {
+    DynamicTheme.of(context).setBrightness(
+        Theme.of(context).brightness == Brightness.dark
+            ? Brightness.light
+            : Brightness.dark);
+  }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Colors.white,
-      ),
-      home: Home(),
-    );
+    return DynamicTheme(
+        defaultBrightness: Brightness.light,
+        data: (brightness) => new ThemeData(
+              primaryColor: Colors.white,
+              primarySwatch: Colors.indigo,
+              appBarTheme: AppBarTheme(
+                color: Colors.white,
+              ),
+              brightness: brightness,
+            ),
+        themedWidgetBuilder: (context, theme) {
+          return new MaterialApp(
+            theme: theme,
+            debugShowCheckedModeBanner: false,
+            home: SplashScreen(),
+          );
+        });
   }
 }
